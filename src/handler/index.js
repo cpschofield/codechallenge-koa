7b1fa2c02989df1ecd7fd6f0d7cb890f980b6533
Chris <chris@simble.io>
2019-03-07 12:20:18 +1100

    added value checks to before assigning value to response object

--- src/handler/index.js
+++ src/handler/index.js
@@ -4,11 +4,13 @@ export const mapShowBriefResponse = async (ctx) => {
   logger.info('mapShowBriefResponse');
   const validRecords = ctx.request.body.payload
     .filter(record => record.drm === true && record.episodeCount > 0)
-    .map(record => ({
-      image: record.image.showImage,
-      slug: record.slug,
-      title: record.title,
-    }));
+    .map((record) => {
+      const obj = { image: null, slug: null, title: null };
+      if (record.image) obj.image = record.image.showImage;
+      if (record.slug) obj.slug = record.slug;
+      if (record.title) obj.title = record.title;
+      return obj;
+    });
   ctx.body = { response: validRecords };
   ctx.status = 200;
 };
