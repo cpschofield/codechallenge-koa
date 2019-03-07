bae11d23832f662242a01c02e6965193d0bd5926
Chris <chris@simble.io>
2019-03-07 13:27:38 +1100

    modified validation rule to require minimum one element in payload array

--- src/validation/index.js
+++ src/validation/index.js
@@ -4,34 +4,36 @@ const seasonItem = joi.object({
   slug: joi.string(),
 });
 
-const payloadItem = joi.object({
-  country: joi.string(),
-  description: joi.string(),
-  drm: joi.boolean(),
-  episodeCount: joi.number(),
-  genre: joi.string(),
-  image: joi.object({
-    showImage: joi.string().required(),
-  }),
-  language: joi.string(),
-  nextEpisode: joi
-    .object({
-      channel: joi.string().allow(null),
-      channelLogo: joi.string(),
-      date: joi.string().allow(null),
-      html: joi.string(),
-      url: joi.string(),
-    })
-    .allow(null),
-  primaryColour: joi.string(),
-  seasons: joi
-    .array()
-    .items(seasonItem)
-    .allow(null),
-  slug: joi.string(),
-  title: joi.string(),
-  tvChannel: joi.string(),
-});
+const payloadItem = joi
+  .object({
+    country: joi.string(),
+    description: joi.string(),
+    drm: joi.boolean(),
+    episodeCount: joi.number(),
+    genre: joi.string(),
+    image: joi.object({
+      showImage: joi.string().required(),
+    }),
+    language: joi.string(),
+    nextEpisode: joi
+      .object({
+        channel: joi.string().allow(null),
+        channelLogo: joi.string(),
+        date: joi.string().allow(null),
+        html: joi.string(),
+        url: joi.string(),
+      })
+      .allow(null),
+    primaryColour: joi.string(),
+    seasons: joi
+      .array()
+      .items(seasonItem)
+      .allow(null),
+    slug: joi.string(),
+    title: joi.string(),
+    tvChannel: joi.string(),
+  })
+  .min(1);
 
 const requestJsonFormatRule = joi
   .object({
